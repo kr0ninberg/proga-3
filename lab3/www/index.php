c5037a6dabcf9908eb8b2843554e34e4c53e15d1
iltl <utypostev@gmail.com>
2023-10-06 12:01:12 +0300

    2.7.1

--- lab3/www/index.php
+++ lab3/www/index.php
@@ -12,11 +12,28 @@
     $url = $_GET['route'] ?? '';
     $routes = require('../src/routes.php');
     //var_dump($routes);
-    $pattern = '~^hello/(.*)$~';
+    //$pattern = '~^hello/(.*)$~';
+
+    $isRouteFound = false;
+    foreach($routes as $pattern=>$controllerAndAction){
+        preg_match($pattern, $url, $matches);
+        if(!empty($matches)){
+            $isRouteFound = true;
+            break;
+        }
+    }if($isRouteFound){
+        $controller = new $controllerAndAction[0];
+
+    }
+
+
+    /*
+    $pattern = $routes[0];
     $controller = new src\Controllers\MainController;
-    preg_match($pattern, $url, $matches);
+    preg_match($pattern, $url, $matches);*/
     //var_dump($matches);
 
+    /*
     if(!empty($matches)){
         $controller->sayHello($matches[1]);
     } else $controller->main();
@@ -27,7 +44,7 @@
         $controller->main();
         return;
     }
-    echo 'not found'
+    echo 'not found'*/
 
     /*if(!empty($_GET['name'])){
         $controller->sayHello($_GET['name']);
